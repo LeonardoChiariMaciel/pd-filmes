1bd1c1353c8ecaa4cce3936373c08de3f93bd04e
Leonardo <leo@lithera.com.br>
2025-05-23 10:20:27 -0300

    mudanca de maquina

---
+++ fetch-filmes.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const API_KEY = '6ab77870d8ea9e9be0c6e259c2c5405f';
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=$6ab77870d8ea9e9be0c6e259c2c5405f&language=pt-BR&page=1`;
+
+fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    const filmes = data.results.slice(0, 5); // Pega os 5 primeiros filmes
+    let filmesHtml = '';
+    filmes.forEach(filme => {
+      filmesHtml += `
+        <li class="caixa-filme">
+          <article class="filme">
+            <img src="https://image.tmdb.org/t/p/w200${filme.poster_path}" alt="${filme.title}">
+            <h2>${filme.title}</h2>
+          </article>
+        </li>
+      `;
+    });
+
+    // Lê o leo.html
+    let html = fs.readFileSync('leo.html', 'utf8');
+    // Substitui o conteúdo da lista de filmes
+    html = html.replace(
+      /<ul id="filmes-lista">[\s\S]*?<\/ul>/,
+      `<ul id="filmes-lista">\n${filmesHtml}\n</ul>`
+    );
+    // Salva o novo HTML
+    fs.writeFileSync('leo.html', html, 'utf8');
+    console.log('Filmes atualizados em leo.html!');
+  })
+  .catch(err => console.error(err));
